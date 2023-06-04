59be6615126bc52d4cf85b8b3602be27e086474c
maryamsalamat <maryamsalamat47@gmail.com>
2023-06-04 17:18:06 +0430

    add counterState and counterComponent

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Provider } from "react-redux";
+import "./App.css";
+import { store } from "./app/store";
+
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
